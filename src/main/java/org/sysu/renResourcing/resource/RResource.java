ad2f3f55a3d5fa5a00824dd7047649bd2a23ad33
Rinkako <jiajidi@163.com>
2017-12-14 18:49:54 +0800

    events class

---
+++ src/main/java/org/sysu/renResourcing/resource/RResource.java
@@ -0,0 +1,12 @@
+package org.sysu.renResourcing.resource;
+
+import org.sysu.renResourcing.entity.RIdentifier;
+
+/**
+ * Author: Rinkako
+ * Date  : 2017/11/22
+ * Usage : Abstract resource class for all kinds of resources
+ */
+public abstract class RResource extends RIdentifier {
+
+}
