6e12ef277b0941fa29e18b3911b062b78323058d
Rinkako <jiajidi@163.com>
2017-12-27 12:15:47 +0800

    COrgan init commit

--- COrgan/Entity/Human.py
+++ COrgan/Entity/Human.py
@@ -5,89 +5,22 @@
 @author : Rinkako
 @time   : 2017/12/26
 """
+from Entity.Worker import Worker
 
 
-class Human:
+class Human(Worker):
     """
     Entity class for `human` data package.
     """
 
-    def __init__(self, humanId, fName, lName):
+    def __init__(self, humanId, fName, lName, note):
         """
         Create new human instance.
         :param humanId: unique human id
         :param fName: first name
         :param lName: last name
+        :param note: description of this human
         """
-        self.Id = humanId
-        self.firstName = fName
-        self.lastName = lName
-        self.roleList = []
-        self.departmentList = []
-        self.capabilityList = []
-
-    def AddRole(self, roleId):
-        """
-        Add a role to this human.
-        :param roleId: role id
-        :return: whether operation success
-        """
-        if roleId in self.roleList:
-            return False
-        self.roleList.append(roleId)
-        return True
-
-    def RemoveRole(self, roleId):
-        """
-        Remove a role from this human.
-        :param roleId: role id
-        :return: whether operation success
-        """
-        if roleId not in self.roleList:
-            return False
-        self.roleList.remove(roleId)
-        return True
-
-    def AddDepartment(self, departmentId):
-        """
-        Add this human to a department.
-        :param departmentId: department id
-        :return: whether operation success
-        """
-        if departmentId in self.departmentList:
-            return False
-        self.departmentList.append(departmentId)
-        return True
-
-    def RemoveDepartment(self, departmentId):
-        """
-        Remove this human from a department.
-        :param departmentId: department id
-        :return: whether operation success
-        """
-        if departmentId not in self.departmentList:
-            return False
-        self.departmentList.remove(departmentId)
-        return True
-
-    def AddCapability(self, capabilityId):
-        """
-        Add a capability to this human.
-        :param capabilityId: capability id
-        :return: whether operation success
-        """
-        if capabilityId in self.capabilityList:
-            return False
-        self.capabilityList.append(capabilityId)
-        return True
-
-    def RemoveCapability(self, capabilityId):
-        """
-        Remove a capability from this human.
-        :param capabilityId: capability id
-        :return: whether operation success
-        """
-        if capabilityId not in self.capabilityList:
-            return False
-        self.capabilityList.remove(capabilityId)
-        return True
+        Worker.__init__(self, humanId, note)
+        self.FirstName = fName
+        self.LastName = lName
