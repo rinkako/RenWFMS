7668b759abfde464b74488a0ee5458b7ce47405e
Rinkako <jiajidi@163.com>
2018-01-16 21:31:02 +0800

    rolemap entity

---
+++ RenNameService/src/main/java/org/sysu/renNameService/GlobalConfigContext.java
@@ -0,0 +1,5 @@
+package org.sysu.renNameService;
+
+public final class GlobalConfigContext {
+
+}
--- RenNameService/src/main/java/org/sysu/renNameService/cluster/RenSupervisor.java
+++ RenNameService/src/main/java/org/sysu/renNameService/cluster/RenSupervisor.java
@@ -1,8 +1,13 @@
 package org.sysu.renNameService.cluster;
 
+import java.util.Date;
+
 /**
  * RenSupervisor
  */
 public class RenSupervisor {
+    private Date lastUpdateTimestamp;
+
+    private String supervisorToken;
 
 }
