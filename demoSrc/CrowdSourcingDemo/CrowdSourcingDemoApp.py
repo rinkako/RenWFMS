a6e4e44c99703a1cb4b2684ee0671ea52b4b01af
Rinkako <jiajidi@163.com>
2018-03-10 21:57:14 +0800

    UI for CrowdSourcing DEMO app

--- demoSrc/CrowdSourcingDemo/CrowdSourcingDemoApp.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-"""
-@module : CrowdSourcingDemoApp
-@author : Rinkako
-@time   : 2018/3/10
-"""
--- demoSrc/CrowdSourcingDemo/GlobalContext.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-# encoding: utf-8
-"""
-@module : GlobalContext
-@author : Rinkako
-@time   : 2018/3/10
-"""
